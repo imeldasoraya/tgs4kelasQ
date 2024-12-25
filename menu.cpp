66ad25f7347e8fb28e5f33180c5c2b6c81243bbb
imeldasoraya <imeldasoraya06@gmail.com>
2024-12-25 11:53:57 +0800

    Proses Akhir

--- menu.cpp
+++ menu.cpp
@@ -13,6 +13,7 @@ float ipk;
 mahasiswa sikc[30];
 
 int pos=-1;
+int p;
 
 void dMenu(){
 system("cls");
@@ -25,56 +26,40 @@ cout<<"5. Exit"<<"\n";
 cout<<"Masukan angka :";        
 }
 
-void tampilkanData(){
-    for (int i = 0; i <= pos; i++) {
-        cout << i+1 << ". " << sikc[i].nim << " , " << sikc[i].nama << " , " << sikc[i].alamat << " , " << sikc[i].ipk << endl;
+void tampilkanData(int p){
+    for (int p = 0; p <= pos; p++) {
+        cout << p+1 << ". " << sikc[p].nim << " , " << sikc[p].nama << " , " << sikc[p].alamat << " , " << sikc[p].ipk << endl;
     }
 }
 
-void masukanData(){
-    pos++;
-    system("cls");
-    fflush(stdin);
-    cout << "masukan nim: ";
-    getline (cin, sikc[pos].nim);
-    cout << "masukan nama: ";
-    getline (cin, sikc[pos].nama);
-    cout << "masukan alamat: ";
-    getline (cin, sikc[pos].alamat);
-    cout << "masukan ipk: ";
-    cin >> sikc[pos].ipk;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
-
-void perbaikanData(){
-    int idx;
-    tampilkanData();
+void perbaikanData(int p){
+    tampilkanData(p); 
     cout << "Masukkan nomor data yang ingin diperbaiki: ";
-    cin >> idx;
-    idx--;
-    if (idx >= 0 && idx <= pos) {
+    cin >> p; 
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    p--; 
+    if (p >= 0 && p <= pos) { 
         system("cls");
         fflush(stdin);
         cout << "masukan nama: ";
-        getline (cin, sikc[idx].nama);
+        getline (cin, sikc[p].nama); 
         cout << "masukan alamat: ";
-        getline (cin, sikc[idx].alamat);
+        getline (cin, sikc[p].alamat); 
         cout << "masukan ipk: ";
-        cin >> sikc[idx].ipk;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> sikc[p].ipk; 
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
     } else {
-        cout << "Nomor tidak valid!" << endl;
+        cout << "Nomor tidak valid!" << endl; 
     }
 }
 
-void hapusData() {
-    int idx;
-    tampilkanData();
+void hapusData(int p) {
+    tampilkanData(p);
     cout << "Masukkan nomor data yang ingin dihapus: ";
-    cin >> idx;
-    idx--;
-    if (idx >= 0 && idx <= pos) {
-        for (int i = idx; i < pos; i++) {
+    cin >> p;
+    p--;
+    if (p >= 0 && p <= pos) {
+        for (int i = p; i < pos; i++) {
             sikc[i] = sikc[i + 1];
         }
         pos--;
@@ -92,37 +77,52 @@ int main() {
         pl = getch();   
 
         switch (pl) {
-            case '1': {
-                masukanData();
-                break;
-            }
-            case '2': {
+            case '1': { 
+                pos++;
                 system("cls");
-                tampilkanData();
+                fflush(stdin);
+                cout << "masukan nim: ";
+                getline (cin, sikc[pos].nim);
+                cout << "masukan nama: ";
+                getline (cin, sikc[pos].nama);
+                cout << "masukan alamat: ";
+                getline (cin, sikc[pos].alamat);
+                cout << "masukan ipk: ";
+                cin >> sikc[pos].ipk;
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                break; 
+            } 
+
+            case '2': { 
+                system("cls");
+                tampilkanData(p);
                 getch();
                 break;
-            }
-            case '3': {
+            } 
+
+            case '3': { 
                 system("cls");
-                perbaikanData();
+                perbaikanData(p);
                 getch();
                 break;
-            }
-            case '4': {
+            } 
+
+            case '4': { 
                 system("cls");
-                hapusData();
+                hapusData(p);
                 getch();
                 break;
-            }
-            case '5':
+            } 
+
+            case '5': 
                 break;
-            default:
+            default: 
                 system("cls");
                 cout << "Pilihan Tidak Tersedia";
                 getch();
                 break;
         }
-    } while (pl != '5');
+    } while (pl != '5'); 
 
     return 0;
 }
